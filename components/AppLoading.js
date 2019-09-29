284ed2eb9ac602e6faab71f0961e5c7fb0c451cf
sreisner <shawn.reisner@gmail.com>
2019-09-29 14:22:59 -0600

    Update app loading UI

--- components/AppLoading.js
+++ components/AppLoading.js
@@ -11,23 +11,68 @@ const StyledAppLoading = styled.div`
   width: 100%;
   height: 100vh;
 
-  h1 {
-    font-size: 72px;
-    background-image: -webkit-linear-gradient(92deg, #f35626, #feab3a);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    animation: hue 1s infinite linear;
-    -webkit-animation: hue 1s infinite linear;
-    line-height: 80px;
+  /* Credit to Tobias Ahlin (https://tobiasahlin.com/spinkit) */
+  .sk-cube-grid {
+    width: 200px;
+    height: 200px;
   }
 
-  @keyframes hue {
-    from {
-      filter: hue-rotate(0deg);
+  .sk-cube-grid .sk-cube {
+    width: 33%;
+    height: 33%;
+    background-color: var(--primary);
+    float: left;
+    -webkit-animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out;
+    animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out;
+  }
+
+  .sk-cube-grid .sk-cube1 {
+    animation-delay: 0.2s;
+  }
+  .sk-cube-grid .sk-cube2 {
+    animation-delay: 0.3s;
+  }
+  .sk-cube-grid .sk-cube3 {
+    animation-delay: 0.4s;
+  }
+  .sk-cube-grid .sk-cube4 {
+    animation-delay: 0.1s;
+  }
+  .sk-cube-grid .sk-cube5 {
+    animation-delay: 0.2s;
+  }
+  .sk-cube-grid .sk-cube6 {
+    animation-delay: 0.3s;
+  }
+  .sk-cube-grid .sk-cube7 {
+    animation-delay: 0s;
+  }
+  .sk-cube-grid .sk-cube8 {
+    animation-delay: 0.1s;
+  }
+  .sk-cube-grid .sk-cube9 {
+    animation-delay: 0.2s;
+  }
+
+  @-webkit-keyframes sk-cubeGridScaleDelay {
+    0%,
+    70%,
+    100% {
+      transform: scale3D(1, 1, 1);
     }
-    to {
-      filter: hue-rotate(-360deg);
+    35% {
+      transform: scale3D(0, 0, 1);
+    }
+  }
+
+  @keyframes sk-cubeGridScaleDelay {
+    0%,
+    70%,
+    100% {
+      transform: scale3D(1, 1, 1);
+    }
+    35% {
+      transform: scale3D(0, 0, 1);
     }
   }
 `;
@@ -35,7 +80,17 @@ const StyledAppLoading = styled.div`
 const AppLoading = () => {
   return (
     <StyledAppLoading>
-      <h1>Loading...</h1>
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1"></div>
+        <div className="sk-cube sk-cube2"></div>
+        <div className="sk-cube sk-cube3"></div>
+        <div className="sk-cube sk-cube4"></div>
+        <div className="sk-cube sk-cube5"></div>
+        <div className="sk-cube sk-cube6"></div>
+        <div className="sk-cube sk-cube7"></div>
+        <div className="sk-cube sk-cube8"></div>
+        <div className="sk-cube sk-cube9"></div>
+      </div>
     </StyledAppLoading>
   );
 };
