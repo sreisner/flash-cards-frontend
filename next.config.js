54b583020dd33d6240323c8c638a65eac1cbe616
sreisner <shawn.reisner@gmail.com>
2019-09-29 20:08:08 -0600

    Add app theming

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const webpack = require('webpack');
-const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withCSS({
+module.exports = withSass({
   webpack: config => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../lib/fontAwesome';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../static/custom.scss';
 
 import { ApolloProvider } from 'react-apollo';
 import App from 'next/app';
@@ -12,16 +12,7 @@ import User from '../components/User';
 import withData from '../lib/withData';
 
 const theme = {
-  red: '#FF0000',
-  black: '#393939',
-  grey: '#3A3A3A',
-  lightgrey: '#E1E1E1',
-  offWhite: '#EDEDED',
-  maroonFlush: '#D62246',
-  wineBerry: '#4B1D3F',
-  surfieGreen: '#0E7C7B',
   maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
 class MyApp extends App {
